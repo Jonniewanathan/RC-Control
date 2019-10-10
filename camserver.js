7dfc6b4a6c52c847f9c7604519e1995c27c244c0
Jonathan <jonathan41@hotmail.com>
2019-10-10 22:36:24 +0100

    Streaming webcam attempt

--- camserver.js
+++ camserver.js
@@ -1,12 +1,16 @@
 let NodeWebcam = require( "node-webcam" );
-let io = require('socket.io')(9090);
+let io = require('socket.io');
+let http = require('http')
+let server = http.createServer();
+server.listen(9090, "http://localhost/");
+let socket = io.listen(server);
 
 let opts = {
     callbackReturn: "base64"
 };
 
 io.sockets.on('connection', function (socket) {
-    console.log()
+    console.log("Connection Established")
     NodeWebcam.capture("test_picture", opts, function (err, data) {
         socket.emit("video", data);
     });
