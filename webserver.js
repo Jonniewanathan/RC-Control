fdfe5679053af50e1a51aaed585186bc93784666
Jonathan <jonathan41@hotmail.com>
2019-10-10 20:35:12 +0100

    Streaming webcam attempt

--- webserver.js
+++ webserver.js
@@ -51,34 +51,34 @@ io.sockets.on('connection', function (socket) {// WebSocket Connection
 
 let socket = io();
 //Default options
-let opts = {
-    //Picture related
-    width: 1280,
-    height: 720,
-    quality: 100,
-    //Delay in seconds to take shot
-    //if the platform supports miliseconds
-    //use a float (0.1)
-    //Currently only on windows
-    delay: 0,
-    //Save shots in memory
-    saveShots: false,
-    // [jpeg, png] support varies
-    // Webcam.OutputTypes
-    output: "jpeg",
-    //Which camera to use
-    //Use Webcam.list() for results
-    //false for default device
-    device: false,
-    // [location, buffer, base64]
-    // Webcam.CallbackReturnTypes
-    callbackReturn: "location",
-    //Logging
-    verbose: false
-};
-//Also available for quick use
-NodeWebcam.capture("test_picture", opts, function(err, data) {
-});
+// let opts = {
+//     //Picture related
+//     width: 1280,
+//     height: 720,
+//     quality: 100,
+//     //Delay in seconds to take shot
+//     //if the platform supports miliseconds
+//     //use a float (0.1)
+//     //Currently only on windows
+//     delay: 0,
+//     //Save shots in memory
+//     saveShots: false,
+//     // [jpeg, png] support varies
+//     // Webcam.OutputTypes
+//     output: "jpeg",
+//     //Which camera to use
+//     //Use Webcam.list() for results
+//     //false for default device
+//     device: false,
+//     // [location, buffer, base64]
+//     // Webcam.CallbackReturnTypes
+//     callbackReturn: "location",
+//     //Logging
+//     verbose: false
+// };
+// //Also available for quick use
+// NodeWebcam.capture("test_picture", opts, function(err, data) {
+// });
 //Return type with base 64 image
 let opts = {
     callbackReturn: "base64"
