7f7eaace3e79b1f06d372ce3523274c4e48f29b2
Jonathan <jonathan41@hotmail.com>
2019-10-17 20:44:10 +0100

    Removed File

--- livecam.js
+++ livecam.js
@@ -2,17 +2,17 @@ const LiveCam = require('livecam');
 const webcam_server = new LiveCam
 ({
     // address and port of the webcam UI
-    'ui_addr' : '127.0.0.1',
+    'ui_addr' : '192.168.1.117',
     'ui_port' : 11000,
 
     // address and port of the webcam Socket.IO server
     // this server broadcasts GStreamer's video frames
     // for consumption in browser side.
-    'broadcast_addr' : '127.0.0.1',
+    'broadcast_addr' : '192.168.1.117',
     'broadcast_port' : 12000,
 
     // address and port of GStreamer's tcp sink
-    'gst_tcp_addr' : '127.0.0.1',
+    'gst_tcp_addr' : '192.168.1.117',
     'gst_tcp_port' : 10000,
 
     // callback function called when server starts
